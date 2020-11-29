06fa1837275ca6ef372cc5b804e5ac8d6fadc14e
nakvanna <nakvannamcu@gmail.com>
2020-11-29 20:48:50 +0700

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/CompositionComponent.vue';
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { ExampleComponent },
+  setup() {
+    const todos = ref<Todo[]>([
+      {
+        id: 1,
+        content: 'ct1'
+      },
+      {
+        id: 2,
+        content: 'ct2'
+      },
+    ]);
+    const meta = ref<Meta>({
+      totalCount: 1200
+    });
+    return { todos, meta };
+  }
+});
+</script>
